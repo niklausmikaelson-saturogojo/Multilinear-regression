8deb35af442c90b883491a4126304858eae0c84b
niklausmikaelson-saturogojo <niklausmikaelson1105@gmail.com>
2026-05-26 12:04:29 +0530

    app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import numpy as np
 import pickle
 
-# ---------------- PAGE CONFIG ----------------
+# ---------------- PAGE CONFIG ---------------
 st.set_page_config(
     page_title="Startup Profit Prediction",
     page_icon="📈",
